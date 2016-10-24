58af60e246b868c53654c62c27c4d0f50170d17f
Hitesh Jasani <hitesh.jasani@gmail.com>
2016-10-24 12:08:38 -0400

    Split emacs modifiable code into separate file.

--- init.el
+++ init.el
@@ -64,23 +64,7 @@
 (add-to-list 'auto-mode-alist '("\\.markdown\\'" . markdown-mode))
 (add-to-list 'auto-mode-alist '("\\.md\\'" . markdown-mode))
 
-
-;; -------------------------------------------------------------------
-;; Customizations
-
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(custom-enabled-themes (quote (sanityinc-solarized-dark)))
- '(custom-safe-themes
-   (quote
-    ("4aee8551b53a43a883cb0b7f3255d6859d766b6c5e14bcb01bed572fcbef4328" default)))
- '(package-selected-packages nil))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
+;;------------------------------------------------------------
+;; Configure custom variables
+(setq custom-file "~/.emacs.d/custom.el")
+(load custom-file)
